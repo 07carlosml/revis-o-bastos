42ce0fb52465b9a361c17709424b651c0e30aeb5
carlos <antoniocarloslimadasilva61@gmail.com>
2024-07-25 09:47:53 -0300

    primeiros passos

---
+++ main.py
@@ -0,0 +1,23 @@
+""" chat - módulo tal que faz isso.
+
+o módulo main faz isso e aquilo. Ele de vez em quando,quase nunca, mais com frequência, parece que não é.
+
+areaq = 1*1
+
+"""
+
+
+
+
+def calcula_area_quadrado():
+
+    lado_do_quadrado: float = 0.0
+
+    lado_do_quadrado =float(input('informe o lado do quadrado'))
+
+    print(lado_do_quadrado*lado_do_quadrado)
+
+    
+
+
+
